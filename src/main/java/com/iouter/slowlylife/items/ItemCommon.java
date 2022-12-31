d498808fb7b7b6df5523f7c4f39b97e50f337ecb
HiddenCirno <2301697863@qq.com>
2022-12-31 22:08:52 +0800

    202212312208

--- src/main/java/com/iouter/slowlylife/items/ItemCommon.java
+++ src/main/java/com/iouter/slowlylife/items/ItemCommon.java
@@ -1,10 +1,26 @@
 package com.iouter.slowlylife.items;
 
 import com.iouter.slowlylife.api.items.ItemBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
+
+import java.util.List;
 
 public class ItemCommon extends ItemBase {
     public ItemCommon(String name) {
         super();
         setUnlocalizedName(name);
+    }
+
+    @Override
+    public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean isF3) {
+        for (int i = 0; ; i++) {
+            String tooltip = StatCollector.translateToLocal(getUnlocalizedName() +".tooltip." + i);
+            if ((getUnlocalizedName() +".tooltip." + i) != tooltip)
+                list.add(StatCollector.translateToLocal(getUnlocalizedName() +".tooltip." + i));
+            else
+                break;
+        }
     }
 }
