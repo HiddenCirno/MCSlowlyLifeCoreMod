8d8d1c14e99671e27423475a8ab210222c4ceb2a
iouter <3155186831@qq.com>
2023-01-25 12:52:37 +0800

    back to ic gui

--- src/main/java/com/iouter/slowlylife/container/SLContainerElectric.java
+++ src/main/java/com/iouter/slowlylife/container/SLContainerElectric.java
@@ -1,26 +1,11 @@
 package com.iouter.slowlylife.container;
 
 import com.iouter.slowlylife.api.tileentity.SLTileEntityElectricBase;
-import ic2.core.ContainerFullInv;
-import ic2.core.slot.SlotArmor;
-import ic2.core.slot.SlotInvSlot;
-import java.util.List;
+import ic2.core.block.wiring.ContainerElectricBlock;
 import net.minecraft.entity.player.EntityPlayer;
 
-public class SLContainerElectric extends ContainerFullInv<SLTileEntityElectricBase> {
+public class SLContainerElectric extends ContainerElectricBlock {
     public SLContainerElectric(EntityPlayer player, SLTileEntityElectricBase tileentity) {
-        super(player, tileentity, 179, 166);
-        for (int i = 0; i < 4; i++) addSlotToContainer(new SlotArmor(player.inventory, i, 152, 5 + i * 18));
-
-        addSlotToContainer(new SlotInvSlot(tileentity.chargeSlot, 0, 128, 14));
-        addSlotToContainer(new SlotInvSlot(tileentity.dischargeSlot, 0, 128, 50));
-    }
-
-    @Override
-    public List<String> getNetworkedFields() {
-        List<String> list = super.getNetworkedFields();
-        list.add("energy");
-        list.add("redstoneMode");
-        return list;
+        super(player, tileentity);
     }
 }
--- src/main/java/com/iouter/slowlylife/gui/SLGuiChargepad.java
+++ src/main/java/com/iouter/slowlylife/gui/SLGuiChargepad.java
@@ -1,85 +1,50 @@
 package com.iouter.slowlylife.gui;
 
-import com.iouter.slowlylife.Tags;
-import com.iouter.slowlylife.container.SLContainerChargepad;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import ic2.core.GuiIconButton;
-import ic2.core.IC2;
+import ic2.core.block.wiring.ContainerElectricBlock;
+import ic2.core.block.wiring.GuiElectricBlock;
+import ic2.core.block.wiring.TileEntityElectricBlock;
 import ic2.core.util.GuiTooltipHelper;
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
-import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
-public class SLGuiChargepad extends GuiContainer {
-    private final SLContainerChargepad container;
+public class SLGuiChargepad extends GuiElectricBlock {
+
+    private final ContainerElectricBlock container;
+    private final String armorInv;
     private final String level;
     private final String name;
-    private static final ResourceLocation BACKGROUND =
-            new ResourceLocation(Tags.MODID + ":textures/gui/gui_storageunit.png");
 
-    public SLGuiChargepad(SLContainerChargepad container, String name) {
+    public SLGuiChargepad(ContainerElectricBlock container, String name) {
         super(container);
-
-        this.xSize = 176;
-        this.ySize = 166;
         this.container = container;
+        this.armorInv = StatCollector.translateToLocal("ic2.EUStorage.gui.info.armor");
         this.level = StatCollector.translateToLocal("ic2.EUStorage.gui.info.level");
         this.name = StatCollector.translateToLocal(name);
     }
 
     @Override
-    public void initGui() {
-        super.initGui();
-        this.buttonList.add(new GuiIconButton(
-                0,
-                (this.width - this.xSize) / 2 + 10,
-                (this.height - this.ySize) / 2 + 7,
-                20,
-                20,
-                new ItemStack(Items.redstone),
-                true));
-    }
-
-    @Override
     protected void drawGuiContainerForegroundLayer(int par1, int par2) {
         this.fontRendererObj.drawString(
-                this.name, (this.xSize - this.fontRendererObj.getStringWidth(this.name)) / 2, 8, 16448255);
-
-        this.fontRendererObj.drawString(this.level, 16, 29, 16448255);
-        int energy = new Double(Math.min((this.container.base).energy, (container.base).maxStorage)).intValue();
-        this.fontRendererObj.drawString(" " + energy, 37, 39, 16448255);
-        this.fontRendererObj.drawString("/" + (container.base).maxStorage, 37, 49, 16448255);
-
-        String output = StatCollector.translateToLocalFormatted("ic2.EUStorage.gui.info.output", container.base.output);
-        this.fontRendererObj.drawString(output, 26, 60, 16448255);
-
+                this.name, (this.xSize - this.fontRendererObj.getStringWidth(this.name)) / 2, 6, 4210752);
+        this.fontRendererObj.drawString(this.armorInv, 8, this.ySize - 126 + 3, 4210752);
+        this.fontRendererObj.drawString(this.level, 79, 25, 4210752);
+        int e = (int) Math.min(((TileEntityElectricBlock) this.container.base).energy, (double)
+                ((TileEntityElectricBlock) this.container.base).maxStorage);
+        this.fontRendererObj.drawString(" " + e, 110, 35, 4210752);
+        this.fontRendererObj.drawString(
+                "/" + ((TileEntityElectricBlock) this.container.base).maxStorage, 110, 45, 4210752);
+        String output = StatCollector.translateToLocalFormatted(
+                "ic2.EUStorage.gui.info.output", ((TileEntityElectricBlock) this.container.base).output);
+        this.fontRendererObj.drawString(output, 85, 60, 4210752);
         GuiTooltipHelper.drawAreaTooltip(
-                par1 - this.guiLeft, par2 - this.guiTop, (container.base).getredstoneMode(), 10, 7, 29, 26);
-    }
-
-    @Override
-    protected void drawGuiContainerBackgroundLayer(float f, int x, int y) {
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.mc.getTextureManager().bindTexture(BACKGROUND);
-        int j = (this.width - this.xSize) / 2; // Good here
-        int k = (this.height - this.ySize) / 2; // good here
-        this.drawTexturedModalRect(j, k, 0, 0, this.xSize, this.ySize); // Renders the actual gui texture.
-        if (container.base.energy > 0.0D) {
-            int i1 = new Double(((176.0f * container.base.getChargeLevel()) / 1.6D) + 5.0D)
-                    .intValue(); // Calculates the current energy in afsu.
-            drawTexturedModalRect(j + 8, k + 73, 0, 251, i1 + 1, 5); // Renders the blue energy bar.
-        }
-    }
-
-    @Override
-    protected void actionPerformed(GuiButton guibutton) {
-        super.actionPerformed(guibutton);
-        if (guibutton.id == 0) IC2.network.get().initiateClientTileEntityEvent(container.base, 0);
+                par1 - this.guiLeft,
+                par2 - this.guiTop,
+                ((TileEntityElectricBlock) this.container.base).getredstoneMode(),
+                153,
+                3,
+                172,
+                22);
     }
 }
